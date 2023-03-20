73b9464d21c5d88606cfe8475dc672f94b0e9b9f
Z1VC0H3N <zivcoh123@gmail.com>
2023-03-20 18:23:49 +0000

    completing task2 , adding memsize.c and changing relevant files

---
+++ user/memsize_test.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int before = memsize();
+    printf("before allocating , size = %d \n", before);
+    void *addr = malloc(sizeof(20000));
+    int after = memsize();
+    printf("after allocating , size = %d \n", after);
+    free(addr);
+    int afterFree = memsize();
+    printf("after free  , size = %d \n", afterFree);
+    exit(0);
+}
